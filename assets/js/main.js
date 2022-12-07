0d7ac593d0d7db0e1e375a214f26638218d05285
zcraber <61133303+zcraber@users.noreply.github.com>
2022-12-08 02:16:53 +0530

    updates
    added dialogue variable, added styling for fonts, improved the display of variables, added sound when clicking the top to scroll button

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,31 @@
 	});
 })();
 
-$(document).ready(function(){
+// scroll body to top on click
+
+$(document).ready(function () {
 	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
+		if ($(this).scrollTop() > 50) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	
+	$('#back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+});
+
+// play sound on click with slight delay
+
+const audio = new Audio("https://files.catbox.moe/qqiej6.ogg");
+const btn = document.getElementById("back-to-top");
+btn.addEventListener("click", function () {
+	setTimeout(function () {
+		audio.play();
+	}, 100);
 });
